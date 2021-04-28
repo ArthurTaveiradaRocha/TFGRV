6dcc6579b9d9d902422359538b7cb584edb8473c
ArthurTaveiradaRocha <arthurt.rocha@yahoo.com.br>
2021-04-28 02:39:27 +0200

    Fixing the formatation

--- immediate_generator/immediate_generator_tb.v
+++ immediate_generator/immediate_generator_tb.v
@@ -1,36 +1,36 @@
 `timescale 1 ns/10 ps
 module immediate_generator_tb();
 
-reg [31:0] Instruction_i;
-wire [31:0] ExtImmediate_o;
+    reg [31:0] Instruction_i;
+    wire [31:0] ExtImmediate_o;
+    
+    immediate_generator UUT(.Instruction_i(Instruction_i), .ExtImmediate_o(ExtImmediate_o));
 
-immediate_generator UUT(.Instruction_i(Instruction_i), .ExtImmediate_o(ExtImmediate_o));
-
-always
-begin
-    Instruction_i = 32'b111111111111111111111111110000011;
-    #20;
-
-    Instruction_i = 32'b000000000000000000000000000010011;
-    #20;
-
-    Instruction_i = 32'b010101010101010101010101000100011;
-    #20;
-
-    Instruction_i = 32'b111111111111111111111111101100111;
-    #20;
-
-    Instruction_i = 32'b000000000000000000000000000110111;
-    #20;
-
-    Instruction_i = 32'b010101010101010101010101011101111;
-    #20;
-
-    Instruction_i = 32'b111111111111111111111111110010111;
-    #20;
-
-    Instruction_i = 32'b000000000000000000000000000000000;
-    #20;
-end
+    always
+    begin
+        Instruction_i = 32'b111111111111111111111111110000011;
+        #20;
+    
+        Instruction_i = 32'b000000000000000000000000000010011;
+        #20;
+    
+        Instruction_i = 32'b010101010101010101010101000100011;
+        #20;
+    
+        Instruction_i = 32'b111111111111111111111111101100111;
+        #20;
+    
+        Instruction_i = 32'b000000000000000000000000000110111;
+        #20;
+    
+        Instruction_i = 32'b010101010101010101010101011101111;
+        #20;
+    
+        Instruction_i = 32'b111111111111111111111111110010111;
+        #20;
+    
+        Instruction_i = 32'b000000000000000000000000000000000;
+        #20;
+    end
 
 endmodule
--- ula/ula.v
+++ ula/ula.v
@@ -1,37 +1,38 @@
 module ula
 (opcode, data1_in, data2_in, funct3, funct7, data_out);
 
-input [31:0] data1_in;   
-input [31:0] data2_in;
-input [6:0] opcode;
-input [2:0] funct3;
-input [6:0] funct7;
-output [31:0] data_out;
-reg [31:0] result;
-reg [17:0] code;
+    input [31:0] data1_in;   
+    input [31:0] data2_in;
+    input [6:0] opcode;
+    input [2:0] funct3;
+    input [6:0] funct7;
+    output [31:0] data_out;
+    reg [31:0] result;
+    reg [17:0] code;
 
-always @(data1_in or data2_in or opcode or funct3 or funct7)
-begin
-  code = {opcode, funct3, funct7};
-  case(code)
-   17'b01100110000000000:
-     result = data1_in + data2_in; //add
-   17'b01100110000100000:
-     result = data1_in - data2_in; //sub
-   17'b01100110010000000:
-     result = data1_in << data2_in; //sll
-   17'b01100111010000000:
-     result = data1_in >> data2_in; //srl
-   17'b01100111000000000:
-     result = data1_in ^ data2_in; //XOR
-   17'b01100111100000000:
-     result = data1_in | data2_in; //OR
-   17'b01100111110000000:
-     result = data1_in & data2_in; //AND
-   default:
-     result = 32'b00000000000000000000000000000000;
-  endcase
-end
+    always @(data1_in or data2_in or opcode or funct3 or funct7)
+    begin
+        code = {opcode, funct3, funct7};
+        case(code)
+            17'b01100110000000000:
+                result = data1_in + data2_in; //add
+            17'b01100110000100000:
+                result = data1_in - data2_in; //sub
+            17'b01100110010000000:
+                result = data1_in << data2_in; //sll
+            17'b01100111010000000:
+                result = data1_in >> data2_in; //srl
+            17'b01100111000000000:
+                result = data1_in ^ data2_in; //XOR
+            17'b01100111100000000:
+                result = data1_in | data2_in; //OR
+            17'b01100111110000000:
+                result = data1_in & data2_in; //AND
+            default:
+                result = 32'b00000000000000000000000000000000;
+      endcase
+    end
 
-assign data_out = result;
+    assign data_out = result;
+    
 endmodule
